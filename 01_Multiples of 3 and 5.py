2b8c78d81bca1db52ad4cd041b03b5207abd6898
Moti666999 <48453182+Moti666999@users.noreply.github.com>
2019-03-30 19:24:39 +0300

    Update Moti Version

--- 01_Multiples of 3 and 5.py
+++ 01_Multiples of 3 and 5.py
@@ -2,3 +2,22 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+
+# Moti Version:
+
+def multiples():
+    num = 1
+    total = []
+
+    while num < 1000:
+        if num % 3 == 0:
+            total.append(num)
+        elif num % 5 == 0:
+            total.append(num)
+        num += 1
+
+    print(sum(total))
+
+
+if __name__ == '__main__':
+    multiples()
